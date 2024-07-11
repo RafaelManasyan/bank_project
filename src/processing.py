dfcba2d149eac128b32f95b2bc8f44aefb8df807
rafaelmanasyan <workpageraf@gmail.com>
2024-07-11 13:25:35 +0300

    написал функцию по сортировке словарей по дате

--- src/processing.py
+++ src/processing.py
@@ -4,12 +4,13 @@ def filter_by_state(date_list: list, state='EXECUTED') -> list:
     return filtered_list
 
 
-def sort_by_date():
-    pass
+def sort_by_date(date_list: list, vector=True) -> list:
+    """Функция сортировки списка словарей по дате"""
+    return sorted(date_list, key=lambda i: i['date'], reverse=vector)
 
 
-print(filter_by_state([
+print(sort_by_date([
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state='CANCELED'))
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
