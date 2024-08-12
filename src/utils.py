0f0ba9d9e688092cf0ad369d83ee47b7683d239b
rafaelmanasyan <workpageraf@gmail.com>
2024-08-12 16:32:33 +0300

    'add test for utils.py and change function get_info'

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,10 @@ import json
 
 
 def get_info(path) -> list:
-    with open(path) as file:
-        py_file = json.load(file)
-    return py_file
+
+    try:
+        with open(path) as file:
+            py_file = json.load(file)
+            return py_file
+    except (FileNotFoundError, json.JSONDecodeError):
+        return ["Ошибка"]
