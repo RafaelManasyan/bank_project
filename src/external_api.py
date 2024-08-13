939224a62ba455f5787205ded1636c4176a2bfb6
rafaelmanasyan <workpageraf@gmail.com>
2024-08-13 17:44:01 +0300

    'add test for utils.py and change function get_info'

--- src/external_api.py
+++ src/external_api.py
@@ -12,9 +12,9 @@ def get_transaction_sum(transaction: dict) -> float:
     if code == "RUB":
         return amount_fiat
     if code == "USD" or code == "EUR":
-        url = f'https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount_fiat}'
-        token_exchange = os.getenv('API_KEY_exchange')
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount_fiat}"
+        token_exchange = os.getenv("API_KEY_exchange")
         headers = {"apikey": token_exchange}
         response = requests.request("GET", url, headers=headers)
         result = dict(response.json())
-        return float(result['result'])
+        return float(result["result"])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,5 +3,4 @@ from src.utils import get_info
 
 
 def test_get_info_file_not_found():
-    assert get_info('') == ['Ошибка']
-
+    assert get_info("") == ["Ошибка"]
